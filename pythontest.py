0e3ad18d47484f6846c94bcb77422e7f69eb4fa4
Julian <huangmeng@gmail.com>
2020-03-01 23:55:54 +0100

    1

---
+++ pythontest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.7
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+
+def qytprint(x):
+    print(x)
+    print(x)
+    print(x)
+
+
+if __name__ == '__main__':
+    qytprint('qytang!')
---
+++ pythontest2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.7
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+test
+
+if __name__ == '__main__':
+    pass
