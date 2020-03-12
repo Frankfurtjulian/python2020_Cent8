569ceb6b7c968e4800d126bd8a063d7f5c4e0d25
Julian <huangmeng@gmail.com>
2020-03-12 15:03:35 +0100

    课堂练习5

---
+++ 005_list_sort.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.7
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+
+l1 = [4, 5, 7, 1, 3, 9, 0]
+l2 = l1.copy()
+l2.sort()
+for i in range(len(l1)):
+    print(l1[i],l2[i])
+
+
+if __name__ == '__main__':
+    pass
