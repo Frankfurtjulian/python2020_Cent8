51106d416173500bee127db4467fe0dcd56adc5c
Julian <huangmeng@gmail.com>
2020-03-14 18:51:25 +0100

    课堂练习12 Ctrl+C for while

---
+++ 012_ctrl_c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.7
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+import time
+
+while True:
+    try:
+        time.sleep(2)
+        print('Ctrl + C to stop')
+    except KeyboardInterrupt:
+        print('Received ctrl+c from Admin!')
+        print('Quit App!')
+
+if __name__ == '__main__':
+    pass
