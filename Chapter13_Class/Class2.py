c3289bf48b6561351180d5aeb362fe3efde59d2d
Julian <huangmeng@gmail.com>
2020-03-18 22:21:53 +0100

    Class练习2--继承类

--- Chapter13_Class/Class2.py
+++ Chapter13_Class/Class2.py
@@ -5,6 +5,16 @@ from Class1 import Person
 
 
 class Manager(Person):
+    """
+    继承Person类,
+    带有自定义加薪的Person
+    继承了通用的lastname， str
+    """
+
+    def __init__(self, name, age, pay):
+        Person.__init__(self, name, age, pay, 'manager')
+        # 为Manager类产生的实例自动产生Job为’Manager‘
+
     def giveraise(self, percent, bonus=0.1):
         Person.giveraise(self, percent + bonus)
         # self.pay *= (1.0 + percent + bonus)
@@ -14,8 +24,8 @@ if __name__ == '__main__':
     tom = Manager(name='Tom Doe', age=50, pay=50000)
     sue = Person(name='Sue Jones', age=45, pay=40000)
     bob = Person('Bob Smith', 42, 30000, 'software')
+    print(tom.job)
     print(tom)
-    print(sue)
     print(bob)
     # print(tom.getlastname())
     # print(bob.getlastname())
