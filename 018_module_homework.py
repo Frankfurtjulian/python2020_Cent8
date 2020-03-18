b9a3c8734c11f2adb98dbd97fba72842ff5e40cc
Julian <huangmeng@gmail.com>
2020-03-18 18:55:50 +0100

    module练习

---
+++ 018_module_homework.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+from qytang_ping import qytang_ping
+from qytang_ssh import qytang_ssh
+
+
+
+
+if __name__ == '__main__':
+    pass
