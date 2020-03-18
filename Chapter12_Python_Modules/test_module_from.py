c425924446e7c6eaa41e69c9c9029f7caca3b880
Julian <huangmeng@gmail.com>
2020-03-18 18:03:53 +0100

    module测试from

---
+++ Chapter12_Python_Modules/test_module_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+from module1 import printer
+printer('Welcome to the home of Zijie Huang!!!!')
+
+if __name__ == '__main__':
+    pass
