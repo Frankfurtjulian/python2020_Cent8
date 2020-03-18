3a84e8766a15e76ba040171d6bcd4821342ab227
Julian <huangmeng@gmail.com>
2020-03-18 18:14:48 +0100

    module测试2

---
+++ Chapter12_Python_Modules/module2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+
+x = 100
+y = [1, 2]
+
+
+if __name__ == '__main__':
+    pass
---
+++ Chapter12_Python_Modules/test_module2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+from module2 import x, y
+print(x)
+print(y)
+
+
+if __name__ == '__main__':
+    pass
