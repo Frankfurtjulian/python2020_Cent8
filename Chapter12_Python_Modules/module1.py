9c7c23fcb544d7f24b52e827d11f9ae821fcf621
Julian <huangmeng@gmail.com>
2020-03-18 18:00:36 +0100

    module测试

---
+++ Chapter12_Python_Modules/module1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+
+
+def printer(x):
+    print(x)
+
+
+if __name__ == '__main__':
+    pass
---
+++ Chapter12_Python_Modules/test_module_import.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+import module1
+
+module1.printer('欢迎来到乾颐堂！！')
+
+if __name__ == '__main__':
+    pass
