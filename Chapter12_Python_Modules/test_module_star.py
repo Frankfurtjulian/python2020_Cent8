23f0ad986e0c63595f5ac4ec3765b86aeeaf6055
Julian <huangmeng@gmail.com>
2020-03-18 18:08:07 +0100

    module测试*

---
+++ Chapter12_Python_Modules/test_module_star.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚本由黄猛编写，用于Python学习！
+from module1 import *
+
+printer('Welcome to the world of Python!!!!')
+
+if __name__ == '__main__':
+    pass
