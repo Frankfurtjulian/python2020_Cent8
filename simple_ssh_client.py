8a709840c89d39c32ea54115a64a34c693f0f484
Julian <huangmeng@gmail.com>
2020-03-21 20:31:37 +0100

    课堂练习，为SSH登录脚本配置选项，调整输出语句，原先的输出语句测试有问题

--- simple_ssh_client.py
+++ simple_ssh_client.py
@@ -2,7 +2,7 @@
 # -*- coding=utf-8 -*-
 # 本脚本由黄猛编写，用于Python学习！
 from qytang_ssh import qytang_ssh
-from arg_parse import ArgumentParser
+from argparse import ArgumentParser
 
 usage = "usage: python Simple_SSH_Client -i ipaddr -u username -p password -c command"
 
@@ -11,10 +11,13 @@ parser = ArgumentParser(usage=usage)
 parser.add_argument("-i", "--ipaddr", dest="ip", help="SSH Server ipv4 Address", default='192.168.213.128', type=str)
 parser.add_argument("-u", "--username", dest="username", help="SSH Username", default='root', type=str)
 parser.add_argument("-p", "--password", dest="password", help="SSH Password", default='root', type=str)
-parser.add_argument("-c", "--command", dest="cmd", help="SSH Password", default='ls', type=str)
+parser.add_argument("-c", "--command", dest="cmd", help="SSH Command", default='ls', type=str)
 
 args = parser.parse_args()
 
+# output = qytang_ssh(args.ip, args.username, args.password, args.cmd)
+# output = [args.ip, args.username, args.password, args.cmd]
 
 if __name__ == '__main__':
-    print(qytang_ssh(args.ip, args.username, args.password, args.cmd))
+    # print(output)
+    print(qytang_ssh(ip=args.ip, username=args.username, password=args.password, cmd=args.cmd))
